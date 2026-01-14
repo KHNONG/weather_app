87ae474bd5a9783935eb9e7120dbab3a362d5f26
KHNONG <khvertnong@gmail.com>
2026-01-14 10:30:04 +0700

    add requiment

--- my_project/settings.py
+++ my_project/settings.py
@@ -124,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFielsStorage'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
